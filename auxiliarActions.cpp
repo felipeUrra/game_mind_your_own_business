4ab644f4d47bcca7e81586f74244990ccdc08613
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-01-22 20:51:40 +0200

    The auxiliar functions were improved and someones were removed.

--- auxiliarActions.cpp
+++ auxiliarActions.cpp
@@ -1,30 +1,12 @@
 #include "auxiliarActions.hpp"
+#include <iostream>
 
-Player get_active_player(Player players[], int active_turn)
-{
-    if (active_turn == players[0].type)
-    {
-        return players[0];
-    }
 
-    return players[1];
-}
-
-Player get_unactive_player(Player players[], Player &active_player)
-{
-    if (active_player.type == 0)
-    {
-        return players[1];
-    }
-
-    return players[0];
-}
-
-bool is_asked_card_in_the_player_hand(Player &player, std::string asked_value)
+bool is_card_in_hand(Player &player, std::string asked_value)
 {
     for (size_t i = 0; i < player.hand.size(); i++)
     {
-        if (player.hand[i].value.compare(asked_value))
+        if (player.hand[i].value == asked_value)
         {
             return true;
         }
@@ -33,32 +15,17 @@ bool is_asked_card_in_the_player_hand(Player &player, std::string asked_value)
     return false;
 }
 
-bool is_asked_card_in_the_opponent_hand(Player &active_player, Player &unactive_player, std::string asked_value)
+bool is_asked_set_in_hand(Player &player, std::string set, size_t &set_index)
 {
-    for (size_t i = 0; i < unactive_player.hand.size(); i++)
+    for (size_t i = 0; i < player.full_sets.cards.size(); i += 4)
     {
-        if (unactive_player.hand[i].value.compare(asked_value))
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-bool is_asked_set_in_the_opponent_hand(Player &active_player, Player &unactive_player, std::string set, size_t &set_index)
-{
-    for (size_t i = 0; i < unactive_player.full_sets.cards.size(); i += 4)
-    {
-        if (unactive_player.full_sets.cards[i].value.compare(set))
+        if (player.full_sets.cards[i].value.compare(set))
         {
             set_index = i;
             return true;
         }
     }
-
-    return false;
-    
+    return false;  
 }
 
 bool is_full_set_in_hand(Player &player, std::string asked_value)
@@ -79,7 +46,7 @@ bool is_full_set_in_hand(Player &player, std::string asked_value)
 
 bool is_drawn_card_equal_to_asked_one(Player &player, std::string asked_value)
 {
-    if (player.hand.back().value.compare(asked_value))
+    if (player.hand.back().value == asked_value)
     {
         return true;
     }
@@ -101,17 +68,17 @@ std::string get_suite_of_card(Card card)
 {
     switch (card.suite)
     {
-    case 0:
-        return "spade";
+    case Card_Suite::Spade:
+        return "♠";
         break;
-    case 1:
-        return "heart";
+    case Card_Suite::Heart:
+        return "♥";
         break;
-    case 2:
-        return "diamond";
+    case Card_Suite::Diamond:
+        return "♦";
         break;
-    case 3:
-        return "club";
+    case Card_Suite::Club:
+        return "♣";
         break;
     default:
         return "none";
--- auxiliarActions.hpp
+++ auxiliarActions.hpp
@@ -3,16 +3,12 @@
 
 #include "player.hpp"
 
-Player get_active_player(Player players[], int active_turn);
-Player get_unactive_player(Player players[], Player &active_player);
 
-bool is_asked_card_in_the_player_hand(Player &player, std::string asked_value);
-bool is_asked_card_in_the_opponent_hand(Player &active_player, Player &unactive_player, std::string asked_value);
-bool is_asked_set_in_the_opponent_hand(Player &active_player, Player &unactive_player, std::string asked_set, size_t &asked_set_index);
+bool is_card_in_hand(Player &player, std::string asked_value);
+bool is_asked_set_in_hand(Player &player, std::string asked_set, size_t &asked_set_index);
 
 bool is_full_set_in_hand(Player &player, std::string asked_value);
 
-bool is_drawn_card_equal_to_asked_one(Player &player, std::string asked_value);
 bool is_deck_empty(Deck &deck);
 
 std::string get_suite_of_card(Card card);
